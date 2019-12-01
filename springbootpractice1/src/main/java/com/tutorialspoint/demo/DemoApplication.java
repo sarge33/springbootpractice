2191cf0e95b58733de9c0ca17f70768edd503d7b
sarge33 <dssdong@gmail.com>
2019-12-01 09:06:54 -0800

    Added docker image build support

--- springbootpractice1/src/main/java/com/tutorialspoint/demo/DemoApplication.java
+++ springbootpractice1/src/main/java/com/tutorialspoint/demo/DemoApplication.java
@@ -1,12 +1,18 @@
 package com.tutorialspoint.demo;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableSwagger2
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
@@ -16,4 +22,11 @@ public class DemoApplication {
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.tutorialspoint.demo")).build();
+    }
+
 }
